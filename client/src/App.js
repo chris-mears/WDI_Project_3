c423ae5699c5000d8c9a0255aece186b6fb76a9e
SadEvilPenguin <mears.chris@gmail.com>
2017-10-13 15:39:18 -0400

    scafolded the react view

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import HomePage from './components/home/HomePage'
+import UserPage from './components/user/UserPage'
+import CarPage from './components/car/CarPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <Router>
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route exact path='/:userId' component={UserPage} />
+                <Route exact path='/:userId/:carId' component={CarPage} />
+            </Switch>
+        </Router>      
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/components/car/CarPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CarPage extends Component {
+    render() {
+        return (
+            <div>
+                Car Page
+            </div>
+        );
+    }
+}
+
+export default CarPage;
