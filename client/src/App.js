dd68e5a46016b2767a8e44e72174fe0341f095d7
SadEvilPenguin <mears.chris@gmail.com>
2017-10-13 15:44:36 -0400

    updated app to include navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './components/home/HomePage'
 import UserPage from './components/user/UserPage'
 import CarPage from './components/car/CarPage'
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
         <Router>
-            <Switch>
+          <div>
+          <NavBar />
+          <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route exact path='/:userId' component={UserPage} />
                 <Route exact path='/:userId/:carId' component={CarPage} />
-            </Switch>
-        </Router>      
+          </Switch>
+          </div>  
+        </Router>    
     )
   }
 }
