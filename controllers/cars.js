2d763e42fdcad14193f16b42cdcc299b94aeb25f
SadEvilPenguin <mears.chris@gmail.com>
2017-10-14 11:17:46 -0400

    updated car controller for create, update and delete

--- controllers/cars.js
+++ controllers/cars.js
@@ -12,4 +12,45 @@ router.get('/:id', async (req, res) => {
     }
   })
 
+  router.post('/', async (req, res) => {
+    try {
+    const user = await User.findById(req.params.userId)
+    const newCar = new Car(req.body)
+    user.cars.push(newCar)
+    const saved = await user.save()
+    res.json(saved)
+    } catch (err) {
+      res.send(err)
+    }
+  })
+
+  router.patch('/:id', async (req, res) => {
+    try {
+    const updatedCar = req.body
+    const user = await User.findById(req.params.userId)
+    const car = user.cars.id(req.params.id)
+    car.make = updatedCar.make
+    car.model = updatedCar.model
+    car.mileage = updatedCar.mileage
+    car.year = updatedCar.year
+    car.tasks = updatedCar.tasks
+    car.reports = updatedCar.reports
+    const saved = await user.save()
+    res.json(saved)
+    } catch (err) {
+      res.send(err)
+    }
+  })
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+    const user = await User.findById(req.params.userId)
+    user.cars.id(req.params.id).remove()
+    const saved = await user.save()
+    res.json(saved)
+    } catch (err) {
+      res.send(err)
+    }
+  })
+
   module.exports = router
