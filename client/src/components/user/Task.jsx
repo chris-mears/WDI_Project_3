eeca175df03651e6900e32ea38131b8ba76422f8
SadEvilPenguin <mears.chris@gmail.com>
2017-10-16 16:35:38 -0400

    added some styling my brain hurts

--- client/src/components/user/Task.jsx
+++ client/src/components/user/Task.jsx
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 import TextField from 'material-ui/TextField';
+import Checkbox from 'material-ui/Checkbox';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import Visibility from 'material-ui/svg-icons/action/visibility';
+import VisibilityOff from 'material-ui/svg-icons/action/visibility-off';
+import IconButton from 'material-ui/IconButton';
 
-const TaskSpan = styled.span`
-  margin: 20px;
+const styles = {
+    block: {
+      maxWidth: 50,
+    },
+    checkbox: {
+      marginBottom: 16,
+    },
+        smallIcon: {
+          width: 36,
+          height: 36,
+        },
+        mediumIcon: {
+          width: 48,
+          height: 48,
+        },
+        largeIcon: {
+          width: 60,
+          height: 60,
+        },
+        small: {
+          width: 72,
+          height: 50,
+          padding: 0,
+        },
+        medium: {
+          width: 96,
+          height: 96,
+          padding: 24,
+        },
+        large: {
+          width: 120,
+          height: 120,
+          padding: 30,
+        }
+  };
 
-  input {
-    font-weight: bold;
-  }
-  input {
-    font-size: 1.2rem;
-    border: none;
-    background-color: initial;
-  }
+const TaskContainer = styled.div `
+    display: flex;
 `
 
 class Task extends Component {
+    state = {
+        checked: false,
+      }
+    
+      updateCheck() {
+        this.setState((oldState) => {
+          return {
+            checked: !oldState.checked,
+          };
+        });
+      }
 
 handleUpdateChange = (event) => {
     this.props.handleTaskChange(event, this.props.carId, this.props.task._id)
@@ -27,10 +71,24 @@ updateTask = (event) => {
 
     render() {
         return (
+        <TaskContainer>
+        <div style={styles.block}>
+        <Checkbox
+          style={styles.checkbox}
+        />
+        </div>
         <TextField onBlur={this.updateTask} 
         onChange={this.handleUpdateChange} 
         name="title" value={this.props.task.title}
+        inputStyle={{height: '50%'}}
         underlineShow={false} />
+        <IconButton
+      iconStyle={styles.mediumIcon}
+      style={styles.small}
+      onClick={() => this.props.deleteTask(this.props.carId, this.props.task._id)}>
+      <i className="material-icons">delete</i>
+    </IconButton>
+        </TaskContainer>
         );
     }
 }
--- client/src/components/user/Tasks.jsx
+++ client/src/components/user/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Task from './Task'
 import {
     Table,
@@ -6,9 +6,9 @@ import {
     TableHeader,
     TableHeaderColumn,
     TableRow,
-    TableRowColumn,
-  } from 'material-ui/Table';
-  import TextField from 'material-ui/TextField';
+    TableRowColumn
+} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
 
 class Tasks extends Component {
     state = {
@@ -18,50 +18,52 @@ class Tasks extends Component {
     }
 
     handleChange = (event) => {
-    const attribute = event.target.name
-    const clonedTask = {...this.state.newTask}
-    clonedTask[attribute] = event.target.value
-    this.setState({newTask: clonedTask})
-  }
+        const attribute = event.target.name
+        const clonedTask = {
+            ...this.state.newTask
+        }
+        clonedTask[attribute] = event.target.value
+        this.setState({newTask: clonedTask})
+    }
 
-  handleSubmit = (event) => {
-      event.preventDefault()
-        this.props.createTask(this.props.carId, this.state.newTask)
-        const resetTask = {title: ''}
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this
+            .props
+            .createTask(this.props.carId, this.state.newTask)
+        const resetTask = {
+            title: ''
+        }
         this.setState({newTask: resetTask})
 
-  }
+    }
     render() {
         return (
             <div>
                 <h4>Tasks:</h4>
-                
-                        <TextField
-                            hintText="New Task"
-                            onChange={this.handleChange}
-                            name="title"
-                            type="text"
-                            value={this.state.newTask.title}
-                            onBlur={this.handleSubmit}/>
-                <Table>
-    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-        <TableRow><TableHeaderColumn>Tasks</TableHeaderColumn></TableRow>
-    </TableHeader>
-    <TableBody displayRowCheckbox={false}>
+
+                <TextField
+                    hintText="New Task"
+                    onChange={this.handleChange}
+                    name="title"
+                    type="text"
+                    value={this.state.newTask.title}
+                    onBlur={this.handleSubmit}/>
+                <div>
                 {this.props.tasks.map((task) => {
-        return (
-        <TableRow key={task._id} selectable={false}>
-        <TableRowColumn>
-            <Task task={task} 
-            handleTaskChange={this.props.handleTaskChange} 
-            updateTask={this.props.updateTask} 
-            carId={this.props.carId} />  
-            <button onClick={() => this.props.deleteTask(this.props.carId, task._id)}>Delete</button>
-            </TableRowColumn>    
-        </TableRow>
-        )})}
-        </TableBody></Table>
-        </div>
+                    return (
+                                    <div key={task._id}>
+                                            <Task
+                                                task={task}
+                                                handleTaskChange={this.props.handleTaskChange}
+                                                updateTask={this.props.updateTask}
+                                                carId={this.props.carId}
+                                                deleteTask={this.props.deleteTask}/>
+                                    </div>
+                                )
+                            })}
+                </div>
+            </div>
         );
     }
 }
