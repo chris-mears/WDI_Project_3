0fe04072a41895cf649cf06cea2071ae7c0f557c
SadEvilPenguin <mears.chris@gmail.com>
2017-10-17 12:48:10 -0400

    updated view functionality

--- client/src/components/user/CarsView.jsx
+++ client/src/components/user/CarsView.jsx
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import axios from 'axios'
 import Car from './Car'
 import Tasks from './Tasks'
 import Reports from './Reports'
 import styled from 'styled-components'
-import Paper from 'material-ui/Paper';
+import Paper from 'material-ui/Paper'
 
 const FlexContainer = styled.div `
 display: flex;
@@ -20,38 +21,55 @@ const style = {
   };
 
 class CarsView extends Component {
+    state = {
+        car: {
+            _id: '',
+           title: '',
+           mileage: '',
+           year: '',
+           tasks: [],
+           reports: []
+        }
+    }
+
+    async componentWillMount() {
+        console.log(this.props.user._id, this.props.carViewId)
+        const res = await axios.get(`/api/users/${this.props.user._id}/cars/${this.props.carViewId}`)
+        this.setState({car: res.data})
+    }
+
+    async componentWillUpdate() {
+        console.log(this.props.user._id, this.props.carViewId)
+        const res = await axios.get(`/api/users/${this.props.user._id}/cars/${this.props.carViewId}`)
+        this.setState({car: res.data})
+    }
     render() {
-        const cars = this.props.user.cars.map(car => {
-                    return (
-                        <Paper style={style} zDepth={2} rounded={false} key={car._id}>
-                                <Car 
-                                car={car} 
-                                user={this.props.user}
-                                deleteCar={this.props.deleteCar} 
-                                handleChange={this.props.handleChange}
-                                updateCar={this.props.updateCar}/>
-                                <Tasks tasks={car.tasks} 
-                                userId={this.props.user._id}
-                                carId={car._id}
-                                deleteTask={this.props.deleteTask}
-                                createTask={this.props.createTask}
-                                handleTaskChange={this.props.handleTaskChange}
-                                updateTask={this.props.updateTask}/>
-                                <Reports reports={car.reports}
-                                userId={this.props.user._id}
-                                carId={car._id}
-                                deleteReport={this.props.deleteReport}
-                                createReport={this.props.createReport}
-                                handleReportChange={this.props.handleReportChange}
-                                updateReport={this.props.updateReport}/>
-                            </Paper>
-                        )
-                })
         return (
-                <FlexContainer>
-                    {cars}
-                </FlexContainer>
-        );
+            <FlexContainer>
+            <Paper style={style} zDepth={2} rounded={false} key={this.state.car._id}>
+                    <Car 
+                    car={this.state.car} 
+                    user={this.props.user}
+                    deleteCar={this.props.deleteCar} 
+                    handleChange={this.props.handleChange}
+                    updateCar={this.props.updateCar}/>
+                    <Tasks tasks={this.state.car.tasks} 
+                    userId={this.props.user._id}
+                    carId={this.state.car._id}
+                    deleteTask={this.props.deleteTask}
+                    createTask={this.props.createTask}
+                    handleTaskChange={this.props.handleTaskChange}
+                    updateTask={this.props.updateTask}/>
+                    <Reports reports={this.state.car.reports}
+                    userId={this.props.user._id}
+                    carId={this.state.car._id}
+                    deleteReport={this.props.deleteReport}
+                    createReport={this.props.createReport}
+                    handleReportChange={this.props.handleReportChange}
+                    updateReport={this.props.updateReport}/>
+                </Paper>
+            </FlexContainer>
+            )
     }
 }
 
