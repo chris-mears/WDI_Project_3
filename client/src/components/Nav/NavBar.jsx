2098c68cce142690b920e745cbce909b222dd631
SadEvilPenguin <mears.chris@gmail.com>
2017-10-17 08:51:01 -0400

    updated navbar and removed drawer

--- client/src/components/Nav/NavBar.jsx
+++ client/src/components/Nav/NavBar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 import DrawerView from './Drawer'
+import LogoIcon from '../LogoIcon'
 
 
 const NavBar = () => (
   <AppBar
     title="CarLog"
-    iconElementLeft={<DrawerView />}
+    iconElementLeft={<LogoIcon />}
+    iconElementRight={
+    <Link to="/">
+    <FlatButton label="Logout">
+    </FlatButton>
+    </Link>
+    }
     zDepth={2}
   />
 );
